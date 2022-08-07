bd04ec77bf4bfac06a02d4b3ad7eebf1c4d77a16
Programist-is-Pochinka <investing_vk@vk.com>
2022-08-07 06:48:28 +0300

    version 2.0

--- js/script.js
+++ js/script.js
@@ -2,13 +2,20 @@ const nav = document.querySelector('nav');
 const navButtons = nav.querySelectorAll('a');
 
 
+const activede = (el) => {
+    const activeButton = document.querySelector('.nav__button_active');
+    activeButton.classList.remove('nav__button_active');
+    el.classList.add("nav__button_active");
+}
+
 navButtons.forEach((btn) => {
     btn.addEventListener('click',(e)=> {
-        const activeButton = document.querySelector('.nav__button_active');
-        activeButton.classList.remove('nav__button_active');
-        btn.classList.add("nav__button_active");
+        activede(btn);
     });
 });
 
+if(window.screen.width >= 1220) {
+    activede(document.querySelector('.nav__button-brends'));
+}
 
 console.log();
